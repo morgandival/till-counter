13afba0107e1f2f9640a0586aa134b19d7fa521d
Morgan Di Val <morgandival@gmail.com>
2021-02-26 17:38:02 +1100

    Added regex expressions for each denomination

--- src/components/Denomination/Denomination.tsx
+++ src/components/Denomination/Denomination.tsx
@@ -10,14 +10,44 @@ function Denomination (props: Props) {
   
   // set the id of the input element for later selection
   const inputID: string = 'denom-' + props.denom.toString();
+  
+  // initialise regex string
   let regex: string = "";
 
+  // check denomination against entered value
   switch (props.denom) {
     case 100:
-      regex = "[0-9]*[0]{2}.[0]{2}";
+      regex = "([0-9]*[0]{2}|0).[0]{2}";
       break;
     case 50:
-      regex = "[0-9]*[0]{2}.[0]{2}";
+      regex = "([0-9]*[05]{1}[0]|0).[0]{2}";
+      break;
+    case 20:
+      regex = "([0-9]*[02468]{1}[0]|0).[0]{2}";
+      break;
+    case 10:
+      regex = "[0-9]*[0].[0]{2}";
+      break;
+    case 5:
+      regex = "[0-9]*[05].[0]{2}";
+      break;
+    case 2:
+      regex = "[0-9]*[02468].[0]{2}";
+      break;
+    case 1:
+      regex = "[0-9]+.[0]{2}";
+      break;
+    case 0.50:
+      regex = "[0-9]+.[05][0]";
+      break;
+    case 0.20:
+      regex = "[0-9]+.[02468][0]";
+      break;
+    case 0.10:
+      regex = "[0-9]+.[0-9][0]";
+      break;
+    case 0.05:
+      regex = "[0-9]+.[0-9][05]";
       break;
   }
 
@@ -28,7 +58,7 @@ function Denomination (props: Props) {
   });
 
   const handleChange = (e: any) => setAmount(e.target.value);
-    
+  
   console.log(amount);
   
 
