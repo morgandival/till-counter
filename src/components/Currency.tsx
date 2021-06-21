79cadf482e5abd0c2ca2ef4904dd247690895fb4
Morgan Di Val <morgandival@gmail.com>
2021-06-21 15:38:48 +1000

    Begun currency selection logic

--- src/components/Currency.tsx
+++ src/components/Currency.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
 function Currency(): JSX.Element {
+  // Initialise currency string
+  const [currency, setCurrency] = React.useState('AUD');
+
+  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setCurrency(() => {
+      return event.target.value;
+    });
+    console.log(currency);
+  };
+
   return (
     <div className="currency" id="currency">
       <label>Choose a currency: </label>
-      <select>
+      <select onChange={handleChange}>
         <option value="AUD">AUD</option>
         <option value="NZD">NZD</option>
         <option value="USD">USD</option>
