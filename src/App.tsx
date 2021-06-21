5e971f12ae2749cf38d3cff6d3941e2173999a31
Morgan Di Val <morgandival@gmail.com>
2021-06-22 04:11:27 +1000

    Accepted raised currency useState, added denomination selection logic

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,35 @@ import Currency from './components/Currency';
 import TillCounter from './components/TillCounter';
 
 function App(): JSX.Element {
+  // Initialise currency
+  const [currency, setCurrency] = React.useState('AUD');
+
+  // Based on currency select option, populate till counter denomiations
+  function fillCurrency(currency: string): Array<number> {
+    // Initialise return array
+    let denominations: Array<number> = [];
+
+    // Fill array based on currency
+    switch (currency) {
+      case 'AUD':
+        denominations = [100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05];
+        break;
+      case 'NZD':
+        denominations = [100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1];
+        break;
+      case 'USD':
+        denominations = [100, 50, 20, 10, 5, 2, 1, 0.5, 0.25, 0.1, 0.05, 0.01];
+        break;
+    }
+
+    return denominations;
+  }
+
   return (
     <div className="App">
       <Header title="Till Counter" />
-      <Currency />
-      <TillCounter denoms={[100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05]} />
+      <Currency currency={currency} setCurrency={setCurrency} />
+      <TillCounter denoms={fillCurrency(currency)} />
     </div>
   );
 }
