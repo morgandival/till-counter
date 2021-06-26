bae86d1dffdc1157f0f4fb4970df2f69666775a6
Morgan Di Val <morgandival@gmail.com>
2021-06-27 02:53:02 +1000

    Added currency interface, changed currency to object consisting of a symbol string and array of number, added euro denominations, fixed other denominations

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,61 @@ import Header from './components/Header';
 import Currency from './components/Currency';
 import TillCounter from './components/TillCounter';
 
+interface Currency {
+  symbol: string;
+  value: Array<number>;
+}
+
 function App(): JSX.Element {
   // Initialise currency
   const [currency, setCurrency] = React.useState('AUD');
 
   // Based on currency select option, populate till counter denomiations
-  function fillCurrency(currency: string): Array<number> {
+  function fillCurrency(currency: string): Currency {
     // Initialise return array
-    let denominations: Array<number> = [];
+    let denominations: Currency = { symbol: '', value: [] };
 
     // Fill array based on currency
     switch (currency) {
       case 'AUD':
-        denominations = [100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05];
+        denominations = {
+          symbol: '$',
+          value: [100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05]
+        };
         break;
       case 'NZD':
-        denominations = [100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1];
+        denominations = {
+          symbol: '$',
+          value: [100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1]
+        };
+        break;
+      case 'EUR':
+        denominations = {
+          symbol: '€',
+          value: [
+            500,
+            200,
+            100,
+            50,
+            20,
+            10,
+            5,
+            2,
+            1,
+            0.5,
+            0.2,
+            0.1,
+            0.05,
+            0.02,
+            0.01
+          ]
+        };
         break;
       case 'USD':
-        denominations = [100, 50, 20, 10, 5, 2, 1, 0.5, 0.25, 0.1, 0.05, 0.01];
+        denominations = {
+          symbol: '$',
+          value: [100, 50, 20, 10, 5, 2, 1, 0.5, 0.25, 0.1, 0.05, 0.01]
+        };
         break;
     }
 
